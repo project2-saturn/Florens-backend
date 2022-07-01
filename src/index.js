5f49782de263d91ad581bf0ce5729ea3134fd05f
psoni01 <psoni01@mylangara.ca>
2022-06-30 21:56:46 -0700

    feat: plant Profile page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
+import ReactDOM from "react-dom"; // installed using npm
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import ReactDOM from 'react-dom'; // installed using npm
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link
-  } from "react-router-dom";
+import React from "react";
 
-import React from 'react';
-
-import HomePage from './HomePage.js';
-import SearchPage from './SearchPage.js';
-import SignUp from './SignUp';
-import Login from './Login';
+import HomePage from "./HomePage.js";
+import SearchPage from "./SearchPage.js";
+import SignUp from "./signup";
+import Login from "./Login";
+import PlantProfile from "./PlantProfile";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/search" element={<SearchPage />}/>
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/plant" element={<PlantProfile />} />
+    </Routes>
+  </BrowserRouter>,
 
-, document.getElementById("react-container"));
+  document.getElementById("react-container")
+);
---
+++ src/lib/PlantProfileImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function(props) {
+    // console.log(props.options);
+  return (
+    <>
+      <div class="secondSecImg">
+              <img src={props.imageSource} alt="plant" />
+            </div>
+    </>
+  );
+}
