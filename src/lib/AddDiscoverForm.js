bdbd1c21b7de221059eb40fadc4b4d72ff5fe599
AD2402 <adandamudi00@mylangara.ca>
2022-07-06 22:16:55 -0700

    feat: add discovery add page

---
+++ src/lib/AddDiscoverForm.js
@@ -0,0 +1,20 @@
+import Footer from "./Footer.js";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+import AWS from "aws-sdk";
+
+const AddDiscoverForm = props => {
+
+return(
+    <div>
+        <form>
+
+        </form>
+    </div>
+);
+}
+
+export default AddDiscoverForm ;
+
--- src/lib/Features.js
+++ src/lib/Features.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function() {
   return (
@@ -37,6 +38,9 @@ export default function() {
             Are you an expert, or have some sort of knowledge? you can
             contribute with our libraryby adding your your research
           </p>
+        </div>
+        <div><Link to = "/addplant">addNew</Link>
+          
         </div>
       </section>
     </>
