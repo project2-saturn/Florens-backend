3f24338dfc91244e3b016e957313956ec82e0e00
AD2402 <adandamudi00@mylangara.ca>
2022-07-11 13:27:55 -0700

    feat: fix css for plant card

--- src/lib/SearchResultCard.js
+++ src/lib/SearchResultCard.js
@@ -9,9 +9,10 @@ export default function(props) {
       <div class="thirdSectionCardList">
         <div class="thirdSectionCardsInner">
           <img class="thirdSectionCardMainImage" src={props.plant.photosURL[0]} alt="plant" />
-          
+          <div className="searchResultNameDesc">
           <h3>{props.plant.name}</h3>
           <p>{props.plant.description.toString().substring(0, 60) + `...`}</p>
+          </div>
           <div className="searchResultCardButtons">
           <Link to="/plant" state={props.plant}>
             <button class="homeThirdSecDetailsButton" type="button">Details</button>
