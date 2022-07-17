76a36b0bec2beb0a6b54e99e0ef8856e964e354c
psoni01 <psoni01@mylangara.ca>
2022-07-17 12:16:56 -0700

    fix: image upload to s3

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const cors = require("cors");
 const { generateToken, verifyToken } = require("./JWT.js");
 const cookies = require("cookie-parser");
 const fetch = require("node-fetch");
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser({limit: '5mb'}));
+
+app.use(bodyParser.json());
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
